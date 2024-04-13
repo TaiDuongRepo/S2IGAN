e90ad08a8b17af40c06e4de379b5e07c0df5bd50
TaiDuongRepo <taiduong1301@gmail.com>
2024-04-13 15:36:26 +0700

    update rdg

--- train_rdg.py
+++ train_rdg.py
@@ -64,10 +64,10 @@ def main(cfg: DictConfig):
     if cfg.ckpt.image_encoder:
         print("Loading Image Encoder state dict...")
         print(image_encoder.load_state_dict(torch.load(cfg.ckpt.image_encoder)))
+        set_non_grad(image_encoder)
     if cfg.ckpt.speech_encoder:
         print("Loading Speech Encoder state dict...")
         print(speech_encoder.load_state_dict(torch.load(cfg.ckpt.speech_encoder)))
-        set_non_grad(image_encoder)
         set_non_grad(speech_encoder)
 
     if multi_gpu:
@@ -166,6 +166,21 @@ def main(cfg: DictConfig):
                 epoch,
                 log_wandb,
             )
+            
+            torch.save({
+                'epoch': epoch,
+                'generator': generator.state_dict(),
+                'discrminator_64': discrminator_64.state_dict(),
+                'discrminator_128': discrminator_128.state_dict(),
+                'discrminator_256': discrminator_256.state_dict(),
+                'relation_classifier': relation_classifier.state_dict(),
+                'image_encoder': image_encoder.state_dict(),
+                'speech_encoder': speech_encoder.state_dict(),
+                'optimizer_generator': optimizer_generator.state_dict(),
+                'optimizer_discrminator': optimizer_discrminator.state_dict(),
+                'optimizer_rs': optimizer_rs.state_dict(),
+            })
+        
     print("Train result:", train_result)
 
 
