4395d39770e76c48384a0571bdea746813b6e0b7
TaiDuongRepo <taiduong1301@gmail.com>
2024-04-14 15:48:41 +0700

    save ckpt rdg

--- train_rdg.py
+++ train_rdg.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 
 import hydra
 import torch
@@ -166,20 +167,34 @@ def main(cfg: DictConfig):
                 epoch,
                 log_wandb,
             )
+
+            save_dir = "/kaggle/working/save_ckpt"
+            if not os.path.exists(save_dir):
+                os.makedirs(save_dir)
             
-            torch.save({
-                'epoch': epoch,
-                'generator': generator.state_dict(),
-                'discrminator_64': discrminator_64.state_dict(),
-                'discrminator_128': discrminator_128.state_dict(),
-                'discrminator_256': discrminator_256.state_dict(),
-                'relation_classifier': relation_classifier.state_dict(),
-                'image_encoder': image_encoder.state_dict(),
-                'speech_encoder': speech_encoder.state_dict(),
-                'optimizer_generator': optimizer_generator.state_dict(),
-                'optimizer_discrminator': optimizer_discrminator.state_dict(),
-                'optimizer_rs': optimizer_rs.state_dict(),
-            })
+            # torch.save({
+            #     'epoch': epoch,
+            #     'generator': generator.state_dict(),
+            #     'discrminator_64': discrminator_64.state_dict(),
+            #     'discrminator_128': discrminator_128.state_dict(),
+            #     'discrminator_256': discrminator_256.state_dict(),
+            #     'relation_classifier': relation_classifier.state_dict(),
+            #     'image_encoder': image_encoder.state_dict(),
+            #     'speech_encoder': speech_encoder.state_dict(),
+            #     'optimizer_generator': optimizer_generator.state_dict(),
+            #     'optimizer_discrminator': optimizer_discrminator.state_dict(),
+            #     'optimizer_rs': optimizer_rs.state_dict(),
+            # })
+            torch.save(speech_encoder.state_dict(), os.path.join(save_dir, "speech_encoder.pt"))
+            torch.save(image_encoder.state_dict(), os.path.join(save_dir, "image_encoder.pt"))
+            torch.save(generator.state_dict(), os.path.join(save_dir, "generator.pt"))
+            torch.save(discrminator_64.state_dict(), os.path.join(save_dir, "discrminator_64.pt"))
+            torch.save(discrminator_128.state_dict(), os.path.join(save_dir, "discrminator_128.pt"))
+            torch.save(discrminator_256.state_dict(), os.path.join(save_dir, "discrminator_256.pt"))
+            torch.save(relation_classifier.state_dict(), os.path.join(save_dir, "relation_classifier.pt"))
+            torch.save(optimizer_generator.state_dict(), os.path.join(save_dir, "optimizer_generator.pt"))
+            torch.save(optimizer_discrminator.state_dict(), os.path.join(save_dir, "optimizer_discrminator.pt"))
+            torch.save(optimizer_rs.state_dict(), os.path.join(save_dir, "optimizer_rs.pt"))
         
     print("Train result:", train_result)
 
